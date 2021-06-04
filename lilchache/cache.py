7b0b36399b452b51acdd85d1cdc7b2a8224d040f
n4cl <devn4cl@gmail.com>
2021-06-04 23:59:03 +0900

    Add: データ取得関数を追加

--- lilchache/cache.py
+++ lilchache/cache.py
@@ -12,8 +12,10 @@ class BaseCache:
     expired: int = 0
     data: dict = dataclasses.field(default_factory=dict)
 
-    def get_data(self):
-        pass
+    def get_data(self, key) -> object:
+        if key in self.data:
+            return self.data[key].body
+        return None
 
     def set_data(self, key: str, content: object) -> None:
         self.data[key] = Content(body=content, generate_time=datetime.now())
