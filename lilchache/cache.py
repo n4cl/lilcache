6cd65c05eb475fcb467cbc239ae26a3db086ceb6
n4cl <devn4cl@gmail.com>
2021-06-04 23:44:55 +0900

    Add: set_data を追加

--- lilchache/cache.py
+++ lilchache/cache.py
@@ -1,14 +1,22 @@
 import dataclasses
+from datetime import datetime
+
+@dataclasses.dataclass
+class Content:
+    body: object = dataclasses.field(default_factory=object)
+    generate_time: datetime = dataclasses.field(default_factory=datetime)
+
 
 @dataclasses.dataclass
 class BaseCache:
     expired: int = 0
+    data: dict = dataclasses.field(default_factory=dict)
 
     def get_data(self):
         pass
 
-    def set_data(self, key):
-        pass
+    def set_data(self, key: str, content: object) -> None:
+        self.data[key] = Content(body=content, generate_time=datetime.now())
 
     def clear_data(self, key):
         pass
